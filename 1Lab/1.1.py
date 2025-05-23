bedafb90b9af74c8ae15d15b9f66df6b448f1790
OkiChitsu <sasharuzhacova@mail.ru>
2025-05-23 22:04:31 +0300

    Add files via upload

---
+++ 1Lab/1.1.py
@@ -0,0 +1,11 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+# TODO заменить значение пропущенного элемента средним арифметическим
+valid_numbers = [num for num in numbers if num is not None]
+
+if not valid_numbers:
+    result = numbers
+else:
+    average = sum(valid_numbers) / len(numbers)
+    result = [average if num is None else num for num in numbers]
+
+print("Измененный список:", result)
---
+++ 1Lab/1.2.py
@@ -0,0 +1,11 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+v_disk = 1.44 * 1024 **2
+lot_pages_in_book = 100
+line_on_page = 50
+symbols_on_line = 25
+symbole_byte = 4
+
+one_book = symbole_byte * symbols_on_line * line_on_page * lot_pages_in_book
+res = v_disk // one_book
+
+print("Количество книг, помещающихся на дискету:", int(res))
